830d1ea0e3be749c710ee7018534a1eb9fb69721
Ehor <123355005+Ehor-developer@users.noreply.github.com>
2023-07-05 20:15:50 +0900

    Update Meet.tsx

--- islands/Meet.tsx
+++ islands/Meet.tsx
@@ -105,7 +105,7 @@ export default function Home() {
                 ) : (
                     <>
                         <input type="date" value={startDate} onChange={handleStartDateChange} className="p-2 mb-2" />
-                        <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded" onClick={handleAnniversaryChange}>記念日を設定する</button>
+                        <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded" onClick={handleAnniversaryChange}>記念日を設定</button>
                     </>
                 )}
 
