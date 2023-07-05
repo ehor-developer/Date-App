3a087a7424c5180919f72fb3299110612d466e36
Ehor <123355005+Ehor-developer@users.noreply.github.com>
2023-07-05 20:16:50 +0900

    Update love.tsx

--- islands/love.tsx
+++ islands/love.tsx
@@ -105,7 +105,7 @@ export default function Home() {
                 ) : (
                     <>
                         <input type="date" value={startDate} onChange={handleStartDateChange} className="p-2 mb-2" />
-                        <button className="bg-pink-500 hover:bg-pink-700 text-white font-bold py-2 px-4 rounded" onClick={handleAnniversaryChange}>記念日を制定する</button>
+                        <button className="bg-pink-500 hover:bg-pink-700 text-white font-bold py-2 px-4 rounded" onClick={handleAnniversaryChange}>記念日を設定する</button>
                     </>
                 )}
 
